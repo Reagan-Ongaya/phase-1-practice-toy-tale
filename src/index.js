ed0acb291d60bd865806ed330bd1caa9ac7a3178
reaganongaya <reagan.ongaya@student.moringaschool.com>
2022-12-15 00:51:51 +0300

    Assignment Completed

--- src/index.js
+++ src/index.js
@@ -4,7 +4,17 @@ document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
   addBtn.addEventListener("click", () => {
-    // hide & seek with the form
+    fetch(url).then(
+      (response) => {
+        console.log(url)
+        return response.json();
+      }
+    ).then(
+      (data) => {
+        console.log(data)
+        return data
+      }
+    )
     addToy = !addToy;
     if (addToy) {
       toyFormContainer.style.display = "block";
